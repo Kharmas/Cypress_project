652571206b1797f018a87ad17c00ddda19e552da
Kharmas <kharmas14@gmail.com>
2021-04-13 21:48:13 +0300

    add 2 lecture code

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -1,76 +1,75 @@
 ///<reference types="Cypress" />
 
-// it('By id', ()=>{
-//     cy.visit("https://facebook.com/")
-//     cy.get('#email')
-// })
-//
-// it('By Class', ()=>{
-//     cy.visit("https://docs.cypress.io/api/commands/blur")
-//     cy.get(".DocSearch-Button-Placeholder")
-// })
-//
-// it('By Tag', ()=>{
-//     cy.visit("https://docs.cypress.io/api/commands/blur")
-//     cy.get("nav")
-// })
-//
-// it('By Tag value', ()=>{
-//     cy.visit("https://facebook.com")
-//     cy.get('[name="pass"]')
-// })
-//
-// it('By different Tag ', ()=>{
-//     cy.visit("https://facebook.com")
-//     cy.get('[data-testid="open-registration-form-button"][role="button"]')
-// })
-//
-// it('By different Types ', ()=>{
-//     cy.visit("https://facebook.com")
-//     cy.get('button[type="submit"][name="login"]')
-// })
-//
-// it.only('By contains name', ()=>{
-//     cy.visit("https://next.privat24.ua/")
-//     cy.get('*[class^="card"]')
-// })
+it('By id', ()=>{
+    cy.visit("https://facebook.com/")
+    cy.get('#email')
+})
 
-// it('By id', ()=>{
-//      cy.visit("http://test.operf.io/#home")
-//      cy.get('#subscribe-email')
-//  })
-//
-// it('By Class', ()=>{
-//     cy.visit("http://test.operf.io/#home")
-//     cy.get('.flex col aic jcc minw-320 padding')
-// })
-//
-// it('By Tag', ()=>{
-//     cy.visit("http://test.operf.io/#home ")
-//     cy.get("footer")
-// })
-//
-// it('By Tag value', ()=>{
-//     cy.visit("http://test.operf.io/#home")
-//     cy.get('[type="text"]')
-// })
-//
-// it('By different Tag ', ()=>{
-//     cy.visit("http://test.operf.io/#home")
-//     cy.get('[data-name="keyboard_arrow_up"][data-color="var(--primary)"]')
-// })
-//
-// it('By different Types ', ()=>{
-//     cy.visit("http://test.operf.io/#home")
-//     cy.get('button[type="submit"]')
-// })
-//
-// it.only('By contains name', ()=>{
-//     cy.visit("http://test.operf.io/#home")
-//     cy.get('*[class^="flex"]*[class$="minw-320"]')
-// })
+it('By Class', ()=>{
+    cy.visit("https://docs.cypress.io/api/commands/blur")
+    cy.get(".DocSearch-Button-Placeholder")
+})
 
-//comment
+it('By Tag', ()=>{
+    cy.visit("https://docs.cypress.io/api/commands/blur")
+    cy.get("nav")
+})
+
+it('By Tag value', ()=>{
+    cy.visit("https://facebook.com")
+    cy.get('[name="pass"]')
+})
+
+it('By different Tag ', ()=>{
+    cy.visit("https://facebook.com")
+    cy.get('[data-testid="open-registration-form-button"][role="button"]')
+})
+
+it('By different Types ', ()=>{
+    cy.visit("https://facebook.com")
+    cy.get('button[type="submit"][name="login"]')
+})
+
+it.only('By contains name', ()=>{
+    cy.visit("https://next.privat24.ua/")
+    cy.get('*[class^="card"]')
+})
+
+it('By id', ()=>{
+     cy.visit("http://test.operf.io/#home")
+     cy.get('#subscribe-email')
+ })
+
+it('By Class', ()=>{
+    cy.visit("http://test.operf.io/#home")
+    cy.get('.flex col aic jcc minw-320 padding')
+})
+
+it('By Tag', ()=>{
+    cy.visit("http://test.operf.io/#home ")
+    cy.get("footer")
+})
+
+it('By Tag value', ()=>{
+    cy.visit("http://test.operf.io/#home")
+    cy.get('[type="text"]')
+})
+
+it('By different Tag ', ()=>{
+    cy.visit("http://test.operf.io/#home")
+    cy.get('[data-name="keyboard_arrow_up"][data-color="var(--primary)"]')
+})
+
+it('By different Types ', ()=>{
+    cy.visit("http://test.operf.io/#home")
+    cy.get('button[type="submit"]')
+})
+
+it.only('By contains name', ()=>{
+    cy.visit("http://test.operf.io/#home")
+    cy.get('*[class^="flex"]*[class$="minw-320"]')
+})
+
 
 it('Using Get with Find and Eq', ()=>{
     cy.visit("https://next.privat24.ua/deposit/open")
