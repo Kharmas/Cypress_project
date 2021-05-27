ae33b9fafe6059ad96542e4008b8e4df329ee258
Kharmas <kharmas14@gmail.com>
2021-05-27 11:02:12 +0300

    add testing request with should

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -36,6 +36,7 @@ it.skip('Money transfer between foreign cards', () => {
 
 });
 
+//Example sending the GET request
 it.skip('Example sending the GET request ', () => {
     cy.request('https://next.privat24.ua')
         .then((response) => {
@@ -43,7 +44,9 @@ it.skip('Example sending the GET request ', () => {
         })
 });
 
-it.skip('Example sending the POST request ', () => {
+
+//Example HTTP POST request with expect verification of response
+it.skip('Example sending the POST request', () => {
 
     const requestBody = {
         "action": "info",
@@ -53,12 +56,12 @@ it.skip('Example sending the POST request ', () => {
         "cardCvv": "444",
         "card": "4552331448138217",
         "cardExp": "0524",
-        "xref": "ba9d22f2a188919041ad2de3f86c21dd",
+        "xref": "7c84bf31d46c9404e88cdde35922d10c",
         "_": 1621515089743
     }
 
     const headersData = {
-        cookie: '_ga=GA1.2.1820646181.1617815081; _gid=GA1.2.2027916834.1621514661; pubkey=863589768c148cec1d34aca44497fe50; _gat_gtag_UA_29683426_11=1; fp=43; lfp=4/7/2021, 8:04:52 PM; pa=1621515071621.71630.24772195076598913next.privat24.ua0.6283673238138388+1'
+        cookie: '_ga=GA1.2.1820646181.1617815081; pubkey=297eea6e70355d6fe8a9605e5e521fbc; _gid=GA1.2.1511398724.1622035664; fp=45; lfp=4/7/2021, 8:04:52 PM; pa=1622035674890.95210.5813687559414678next.privat24.ua0.8260377293358108+1'
     }
 
 
@@ -68,53 +71,43 @@ it.skip('Example sending the POST request ', () => {
         body: requestBody,
         headers: headersData
     }).then((response) => {
-        console.log(response.body);
+            expect(response).to.have.property('status').to.equal(200)
+            expect(response.body).to.have.property('status').to.equal('success')
+            expect(response.body.data).to.have.property('amount').to.equal('50.0')
+        console.log(response);
     })
 });
 
-it.skip('Test POST', ()=>{
+//Example HTTP POST request with should verification of response
+it('Example sending the POST request', () => {
 
-    const testBody = {
-        title: 'foo',
-        body: 'bar',
-        userId: 1,
+    const requestBody = {
+        "action": "info",
+        "phone": "+380665424680",
+        "amount": 50,
+        "currency": "UAH",
+        "cardCvv": "444",
+        "card": "4552331448138217",
+        "cardExp": "0524",
+        "xref": "66af4bf14289216afd85e5a5211d22ca",
+        "_": 1621515089743
     }
 
-    const testHeaderData = {
-        'Content-type': 'application/json; charset=UTF-8',
+    const headersData = {
+        cookie: '_ga=GA1.2.1820646181.1617815081; _gid=GA1.2.1511398724.1622035664; pubkey=52f24d2a023abe84933c355a7b0237a1; _gat_gtag_UA_29683426_11=1; fp=48; lfp=4/7/2021, 8:04:52 PM; pa=1622035674890.95210.5813687559414678next.privat24.ua0.8260377293358108+3'
     }
+
 
     cy.request({
         method: 'POST',
-        url: 'https://jsonplaceholder.typicode.com/posts',
-        body: testBody,
-        headers: testHeaderData
-    }).then((responce) => {
-        console.log(responce.body)
+        url: 'https://next.privat24.ua/api/p24/pub/mobipay',
+        body: requestBody,
+        headers: headersData
+    }).its('body').should('contain', {
+        status: 'success'
+    }).its('data').should('contain', {
+        status: 'ok'
     })
+});
 
-})
-
-it('Test update', ()=>{
-
-    const testHeader = {
-        'Content-type': 'application/json; charset=UTF-8'
-    }
-
-    const testBody = {
-        id: 1,
-        title: 'foo',
-        body: 'bar',
-        userId: 1,
-    }
-
-    cy.request({
-        method: 'PUT',
-        url: 'https://jsonplaceholder.typicode.com/posts/1',
-        body: testBody,
-        headers: testHeader
-    }).then((responce) =>{
-        console.log(responce.body)
-    })
-})
 
